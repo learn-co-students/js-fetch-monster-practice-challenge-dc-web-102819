52e50077c2c88aeae8ccab26ea4eb4bfe2480c1a
jdromero88 <github email address>
2019-12-15 12:21:20 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,47 +1,79 @@
+var totalMonsters;
+var totalPages;
 document.addEventListener('DOMContentLoaded', webIsReady)
 
 function webIsReady() {
   console.log('DOM is loaded')
-  getMonsters(50)
+  getMonsters()
   getMonsterForm().addEventListener('submit', sendMonster)
+  getBtnBackListener().addEventListener('click', previousPage)
+  getBtnNextListener().addEventListener('click', nextPage)
 }
 
 function getMonsterForm() {
   return document.querySelector('#monster-form')
 }
 
-function getMonsters(number) {
-  // console.log('Im going to load something. Number is:' + number);
-  fetch(`http://localhost:3000/monsters/?_limit=${number}`)
-  .then(response => response.json())
-  .then(monsterData => showMonsters(monsterData))
+function getBtnBackListener() {
+  return document.querySelector('#previous')
 }
 
-function showMonsters(monsterData) {
-  monsterData.forEach(
-    data => {
-      const divMonsterContainer = document.querySelector('#monster-container')
-      // creating elements
-      let monsterBox = document.createElement('div')
-      let h3TagName = document.createElement('h3')
-      let h3TagAge = document.createElement('h3')
-      let h3TagDescription = document.createElement('h3')
-      let pTagDescription = document.createElement('p')
+function previousPage(e) {
+  document.querySelector('#monster-container-remove').innerText = ''
+  let page = Number(getCurrentPage().innerText)
+  page -= 1
+  getMonsters(page)
+}
 
-      // seting the data
-      h3TagName.innerText = 'Name: ' + data.name
-      h3TagAge.innerText = 'Age: ' + data.age
-      h3TagDescription.innerText = 'Description:'
-      pTagDescription.innerText = data.description
+function getBtnNextListener() {
+  return document.querySelector('#next')
+}
 
-      // appending elements
-      divMonsterContainer.appendChild(monsterBox)
-      monsterBox.appendChild(h3TagName)
-      monsterBox.appendChild(h3TagAge)
-      monsterBox.appendChild(h3TagDescription)
-      monsterBox.appendChild(pTagDescription)
-    }
-  )
+function nextPage(e) {
+  let page = Number(getCurrentPage().innerText)
+  document.querySelector('#monster-container-remove').innerText = ''
+  page += 1
+  console.log(page);
+  getMonsters(page)
+}
+
+function getCurrentPage() {
+  return document.querySelector('#current-page')
+}
+
+function getMonsters(page=1, limit=1000) {
+  getTotalMonsters()
+  getCurrentPage().innerText = page
+
+  console.log('Page is: ' + page);
+  if (page === 1) {
+    document.querySelector('#previous').disabled = true
+  } else {
+    document.querySelector('#previous').disabled = false
+  }
+  if (page >= totalPages){
+    console.log('page is:' + page + ' totalPages is:' + totalPages);
+    document.querySelector('#next').disabled = true
+  } else {
+    document.querySelector('#next').disabled = false
+  }
+  fetch(`http://localhost:3000/monsters/?_limit=${limit}&_page=${page}`)
+  .then(response => response.json())
+  .then(monsterData => monsterData.forEach(
+    data => renderMonster(data)
+  ))
+  .catch(error => console.log(error.message))
+}
+
+function getTotalMonsters() {
+  fetch('http://localhost:3000/monsters/')
+  .then(response => response.json())
+  .then(monsterData => {
+    totalMonsters = monsterData.length
+    totalPages = totalMonsters / 1000
+    console.log('totalPages'+totalPages);
+  })
+
 }
 
 function sendMonster(event) {
@@ -50,14 +82,14 @@ function sendMonster(event) {
   let monsterName = document.querySelector('#monster-name').value
   let monsterAge = document.querySelector('#monster-age').value
   let monsterDescription = document.querySelector('#monster-description').value
-  createMonster(monsterName, monsterAge, monsterDescription)
+  if (monsterName === '' || monsterAge === '' || monsterDescription === '' ) {
+    alert('Must insert Name, Age and Description')
+  } else {
+    createMonster(monsterName, monsterAge, monsterDescription)
+  }
 }
 
 function createMonster(name, age, description) {
-  console.log(
-    'This is the data to insert in DB. Name: ' + name
-  )
-  debugger
   let data = {
     'name': name,
     'age': parseInt(age),
@@ -75,12 +107,36 @@ function createMonster(name, age, description) {
 
   fetch('http://localhost:3000/monsters', conObj)
   .then(response => response.json())
-  .then(postMonster => doSomethingElse(postMonster))
+  .then(newMonster => renderMonster(newMonster))
   .catch(err => console.log(err.message))
 
   getMonsterForm().reset()
 }
 
-function doSomethingElse(newMonster) {
-  console.log(newMonster);
+function renderMonster(newMonster) {
+  let divMonsterContainerRemove = document.createElement('div')
+  divMonsterContainerRemove.setAttribute('id', 'monster-container-remove')
+  const divMonsterContainer = document.querySelector('#monster-container')
+  divMonsterContainer.appendChild(divMonsterContainerRemove)
+
+  divMonsterContainerRemove = document.querySelector('#monster-container-remove')
+
+  let monsterBox = document.createElement('div')
+  let h3TagName = document.createElement('h3')
+  let h3TagAge = document.createElement('h3')
+  let h3TagDescription = document.createElement('h3')
+  let pTagDescription = document.createElement('p')
+
+  // seting the data
+  h3TagName.innerText = 'Name: ' + newMonster.name
+  h3TagAge.innerText = 'Age: ' + newMonster.age
+  h3TagDescription.innerText = 'Description:'
+  pTagDescription.innerText = newMonster.description
+
+  // appending elements
+  divMonsterContainerRemove.appendChild(monsterBox)
+  monsterBox.appendChild(h3TagName)
+  monsterBox.appendChild(h3TagAge)
+  monsterBox.appendChild(h3TagDescription)
+  monsterBox.appendChild(pTagDescription)
 }
